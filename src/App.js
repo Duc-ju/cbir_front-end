bb35468780f1ac6c8e1ed70e505d1de693fc6067
Duc-ju <Trangduc56@gmail.com>
2022-06-21 22:00:17 +0700

    update title and icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Main from "./components/main";
 
 function App() {
+  useEffect(() => {
+    document.title = "Hệ thống tìm kiếm ảnh món ăn";
+  }, []);
   return (
     <div className="App">
       <Header />
